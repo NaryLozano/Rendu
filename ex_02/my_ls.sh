1dbd622c9d9365f5132324198209438ea9af2940
Nary <narylozano@gmail.com>
2021-03-01 16:30:54 +0100

    ex_02 finished

---
+++ ex_02/my_ls.sh
@@ -0,0 +1,7 @@
+
+ls -lamp
+
+# ls - show you the directories
+# ls -a show you the hidden files
+# ls -am show you the hidden files separated y comas
+# ls -amp show you the hidden files separated by comes and with a slash at the e#end
