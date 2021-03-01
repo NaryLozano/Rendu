ca38c380580baae4b9d5c17409af01be009794ca
Nary <narylozano@gmail.com>
2021-03-01 16:05:40 +0100

    ls script

--- ex_01/my_ls.sh
+++ ex_01/my_ls.sh
@@ -1,2 +1,6 @@
 
 ls -lamp
+# ls - show you the directories
+# ls -a show you the hidden files
+# ls -am show you the hidden files separated y comas
+# ls -amp show you the hidden files separated by comes and with a slash at the e#end
